9232514567407c96c7a7f499ceaa8fde36bac91e
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2021-03-09 21:02:34 -0800

    Create Shard_query.py
    Starting coding

---
+++ Shard_query.py
@@ -0,0 +1,10 @@
+import pymysql
+
+db = pymysql.connect(host="10.0.0.104", port=4000, user="maxuser", passwd="maxpwd")
+cursor = db.cursor()
+
+print('The last 10 rows of zipcodes_one are:')
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 9990,10;")
+results = cursor.fetchall()
+for result in results:
+    print(result)
