c55acb5d6d133a9449a343e40e7e34a05752daae
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2021-03-10 00:27:37 -0800

    Edit shard.sql files

--- maxscale/sql/master/shard1.sql
+++ maxscale/sql/master/shard1.sql
@@ -1,3 +1,13 @@
+RESET MASTER;
+
+CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
+CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
+GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
+
+SET GLOBAL max_connections=10000;
+SET GLOBAL gtid_strict_mode=ON;
+
 -- phpMyAdmin SQL Dump
 -- version 4.7.9
 -- https://www.phpmyadmin.net/
